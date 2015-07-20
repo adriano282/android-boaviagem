cd54cccf494e7d1fede1d7ac2e379d189513ab3d
Adriano de Jesus <adriano.jesus282@gmail.com>
2015-07-20 17:11:02 -0300

    Modificações para realização de Login

--- app/src/main/java/boaviagem/casadocodigo/com/br/boaviagem/BoaViagemActivity.java
+++ app/src/main/java/boaviagem/casadocodigo/com/br/boaviagem/BoaViagemActivity.java
@@ -5,18 +5,36 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class BoaViagemActivity extends Activity {
+    private EditText usuario;
+    private EditText senha;
+
 
     public void entrarOnClick(View view) {
+        String usuarioInformado = usuario.getText().toString();
+        String senhaInformada = senha.getText().toString();
 
+        if ("leitor".equals(usuarioInformado) &&
+                "123".equals(senhaInformada)) {
+
+        } else {
+            String mensagemErro = getString(R.string.erro_autenticacao);
+            Toast toast = Toast.makeText(this, mensagemErro,
+                    Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boa_viagem);
+        usuario = (EditText) findViewById(R.id.usuario);
+        senha = (EditText) findViewById(R.id.senha);
     }
 
 
