2ccb7e4d1ac8c8fb309e40c8798c384840f35151
Adriano de Jesus <adriano.jesus282@gmail.com>
2015-07-23 00:02:34 -0300

    Creation of ViagemListActivity

---
+++ app/src/main/java/boaviagem/casadocodigo/com/br/boaviagem/ViagemListActivity.java
@@ -0,0 +1,70 @@
+package boaviagem.casadocodigo.com.br.boaviagem;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class ViagemListActivity extends ListActivity implements OnItemClickListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listarViagens()));
+        ListView listView = getListView();
+        listView.setOnItemClickListener(this);
+    }
+
+    private List<String> listarViagens() {
+        return Arrays.asList("São Paulo", "Bonito", "Maceio");
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view,
+                            int position, long id) {
+        TextView textView = (TextView) view;
+        String mensagem = "Viagem selecionada: " + textView.getText();
+        Toast.makeText(getApplicationContext(), mensagem,
+                Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(this, GastoListActivity.class));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
