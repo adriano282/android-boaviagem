89d213fe76e1153ab3d301414607f7e239d36b38
Adriano de Jesus <adriano.jesus282@gmail.com>
2015-07-25 20:47:59 -0300

    Criação e configuração da classe interna GastoViewBind

--- app/src/main/java/boaviagem/casadocodigo/com/br/boaviagem/GastoListActivity.java
+++ app/src/main/java/boaviagem/casadocodigo/com/br/boaviagem/GastoListActivity.java
@@ -4,40 +4,101 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.SimpleAdapter.ViewBinder;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by adriano on 24/07/15.
  */
 public class GastoListActivity extends ListActivity implements AdapterView.OnItemClickListener {
+    private List<Map<String, Object>> gastos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setListAdapter(new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, listarGastos()));
-        ListView listView = getListView();
-        listView.setOnItemClickListener(this);
+        String[] de = {"data", "descricao", "valor", "categoria"};
+        int[] para = {R.id.data, R.id.descricao, R.id.valor, R.id.categoria };
+
+        SimpleAdapter adapter = new SimpleAdapter(this,
+                listarGastos(), R.layout.lista_gasto, de, para);
+
+        adapter.setViewBinder(new GastoViewBinder());
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view,
                             int position, long id) {
-        TextView textView = (TextView) view;
-        Toast.makeText(this, "Gasto selecionado: " + textView.getText(),
+        Map<String, Object> map = gastos.get(position);
+        String descricao = (String) map.get("descricao");
+        String mensagem = "Gasto selecionado: " + descricao;
+        Toast.makeText(this, "Gasto selecionado: " + mensagem,
                 Toast.LENGTH_SHORT).show();
     }
 
-    private List<String> listarGastos() {
-        return Arrays.asList("Sanduiche R$ 19,90",
-                "Taxi Aeroporto - Hotel R$ 34,00",
-                "Revista R$ 12,00");
+
+    private List<Map<String, Object>> listarGastos() {
+        gastos = new ArrayList<Map<String, Object>>();
+
+        Map<String, Object> item = new HashMap<String, Object>();
+        item.put("data", "04/02/2012");
+        item.put("descricao", "Diaria Hotel");
+        item.put("valor", "RS 260,00");
+        item.put("categoria", R.color.categoria_hospedagem);
+        gastos.add(item);
+
+        item = new HashMap<String, Object>();
+        item.put("data", "04/02/2012");
+        item.put("descricao", "Almoco Restaurante");
+        item.put("valor", "R$ 120,00");
+        item.put("categoria", R.color.categoria_alimentacao);
+        gastos.add(item);
+
+        item = new HashMap<String, Object>();
+        item.put("data", "05/02/2012");
+        item.put("descricao", "Diaria Hotel");
+        item.put("valor", "RS 260,00");
+        item.put("categoria", R.color.categoria_hospedagem);
+        gastos.add(item);
+
+        return gastos;
+    }
+
+    private String dataAnterior = "";
+
+    private class GastoViewBinder implements ViewBinder {
+
+        @Override
+        public boolean setViewValue(View view, Object data, String textRepresentation) {
+
+            if (view.getId() == R.id.data) {
+
+                if (!dataAnterior.equals(data)) {
+                    TextView textView = (TextView) view;
+                    textView.setText(textRepresentation);
+                    dataAnterior = textRepresentation;
+                    view.setVisibility(View.VISIBLE);
+                } else {
+                    view.setVisibility(View.GONE);
+                }
+                return true;
+            }
+
+            if (view.getId() == R.id.categoria) {
+                Integer id = (Integer) data;
+                view.setBackgroundColor(getResources().getColor(id));
+                return true;
+            }
+            return false;
+        }
     }
 }
