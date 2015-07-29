15b479d215a69606897ea677d90325f4037fadf2
Adriano <adriano.jesus2@fatec.sp.gov.br>
2015-07-29 17:17:57 -0300

    Criaçao de constantes referentes as tabelas do banco de dados

--- app/src/main/java/boaviagem/casadocodigo/com/br/boaviagem/dao/DatabaseHelper.java
+++ app/src/main/java/boaviagem/casadocodigo/com/br/boaviagem/dao/DatabaseHelper.java
@@ -11,6 +11,34 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE = "GoodTravel";
     private static int VERSION = 1;
 
+    public static class Travel {
+        public static final String TABLE = "viagem";
+        public static final String _ID = "_id";
+        public static final String DESTINY = "destino";
+        public static final String DATE_ARRIVE = "data_chegada";
+        public static final String DATE_OUT = "data_saida";
+        public static final String BUDGET = "orcamento";
+        public static final String QUANTITY_PERSONS = "quantidade_pessoas";
+        public static final String TYPE_TRAVEL = "tipo_viagem";
+        public static final String[] COLUNAS = new String[] {
+          _ID, DESTINY, DATE_ARRIVE, DATE_OUT, TYPE_TRAVEL, BUDGET, QUANTITY_PERSONS};
+
+    }
+
+    public static class Spent {
+        public static final String
+                TABLE = "spent",
+                _ID = "_id",
+                TRAVEL_ID = "viagem_id",
+                CATEGORY = "categoria",
+                DATE = "data",
+                DESCRIPTION = "descricao",
+                VALUE = "valor",
+                PLACE = "local";
+        public static final String[] COLUNAS = new String[] {
+                _ID, TRAVEL_ID, CATEGORY, DATE, DESCRIPTION, VALUE, PLACE};
+    }
+
     public DatabaseHelper(Context context) {
         super(context, DATABASE, null, VERSION);
     }
